0b4f0b58b2efcfe573d928831fc51cd658dc7b39
Chris Ashman <chrisashman96@gmail.com>
2020-03-23 19:31:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,5 +7,6 @@ class Pokemon
   
   def self.find(id_num, db)
     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info)
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], 
+    type: pokemon_info)
 end
